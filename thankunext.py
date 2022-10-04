933c16cf319212d958898199204e0475e530cb81
Guilherme <constshi0n@proton.me>
2022-10-03 22:49:01 -0300

    improvement: melhora pattern

--- thankunext.py
+++ thankunext.py
@@ -5,7 +5,8 @@ import sys
 import requests
 
 if not len(sys.argv) == 2:
-    exit(f'error: invalid arguments\nusage: python {sys.argv[0]} http://example.com')
+    exit(
+        f'error: invalid arguments\nusage: python {sys.argv[0]} http://example.com')
 
 url = sys.argv[1]
 
@@ -16,7 +17,7 @@ response = requests.get(url, headers={
     'user-agent': 'thankunext/1.0'
 })
 
-if not (build_manifest_path := re.search(r'/_next/static/[a-zA-Z0-9]+/_buildManifest\.js', response.text)):
+if not (build_manifest_path := re.search(r'/_next/static/[\w-]+/_buildManifest\.js', response.text)):
     exit('error: _buildManifest.js was not found. is this site really running Next.js?')
 
 build_manifest_path = build_manifest_path.group()
@@ -24,7 +25,8 @@ build_manifest_response = requests.get(url + build_manifest_path, headers={
     'user-agent': 'thankunext/1.0'
 })
 
-paths = re.findall(r'(?<=\")[a-zA-Z0-9_/\[\]\.]+(?=\")', build_manifest_response.text)
+paths = re.findall(
+    r'(?<=\")[a-zA-Z0-9_/\[\]\.]+(?=\")', build_manifest_response.text)
 
 paths = sorted(set(paths))
 
